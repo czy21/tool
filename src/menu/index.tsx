ea7b2f2c40f1b0fc8b5ce5c85dd6408d84d2cf2e
Bruce CHEN <805899926@qq.com>
2024-02-23 12:13:05 +0800

    # add cdn page

--- src/menu/index.tsx
+++ src/menu/index.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import stub from "@/init";
 
 export interface MenuModel {
     name: string,
--- src/view/cf-best/cdn/index.tsx
+++ src/view/cf-best/cdn/index.tsx
@@ -1,12 +1,67 @@
 import React from "react";
+import stub from "@/init"
 
+const columns = [
+    {
+        title: 'ip',
+        dataIndex: 'valueStr',
+        key: 'valueStr',
+    },
+    {
+        title: '国家',
+        dataIndex: 'country',
+        key: 'country',
+    },
+    {
+        title: '国家代码',
+        dataIndex: 'countryCode',
+        key: 'countryCode',
+    },
+    {
+        title: '城市',
+        dataIndex: 'city',
+        key: 'city',
+    },
+    {
+        title: '区域',
+        dataIndex: 'regionName',
+        key: 'regionName',
+    },
+    {
+        title: 'ISP',
+        dataIndex: 'isp',
+        key: 'isp',
+    },
+    {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+    },
+];
 
 const CFBestCDN: React.FC = () => {
 
+    const [data, setData] = stub.ref.react.useState<any>({})
+    const [query, setQuery] = stub.ref.react.useState<any>({})
+
+    stub.ref.react.useEffect(() => handleSearch(query), [query])
+
+    const handleSearch = (q?: any) => {
+        setQuery(q)
+        stub.api.post("cf-best/cdn/page", stub.ref.lodash.omit(q, "total")).then((t: any) => setData(t.data.data))
+    }
+
     return (
-        <div>
-            cdn
-        </div>
+        <stub.ref.antd.Table dataSource={data.list} columns={columns}
+                             pagination={{
+                                 total: data.total,
+                                 current: data.page,
+                                 pageSize: data.pageSize,
+                                 showTotal: ((t: any, r: any) => `第 ${r[0]}-${r[1]} 条/总共 ${t} 条`),
+                                 onChange: (pageIndex, pageSize) =>
+                                     setQuery({...query, "page": pageIndex, "pageSize": pageSize})
+                             }}
+        />
     )
 }
 
