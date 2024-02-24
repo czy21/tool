efc764be31833e0cbb9a3c8624d4ca0ba844c703
Bruce Chen <805899926@qq.com>
2024-02-24 10:01:25 +0800

    #

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
