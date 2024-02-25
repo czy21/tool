77089e63062797a6a80fb42dce0b218713f9c4a0
Bruce CHEN <805899926@qq.com>
2024-02-25 22:17:40 +0800

    # cleanup

--- src/view/cf-best/cdn/index.tsx
+++ src/view/cf-best/cdn/index.tsx
@@ -40,39 +40,6 @@ const columns = [
     },
 ];
 
-const treeData = [
-    {
-        value: 'parent 1',
-        title: 'parent 1',
-        children: [
-            {
-                value: 'parent 1-0',
-                title: 'parent 1-0',
-                children: [
-                    {
-                        value: 'leaf1',
-                        title: 'my leaf',
-                    },
-                    {
-                        value: 'leaf2',
-                        title: 'your leaf',
-                    },
-                ],
-            },
-            {
-                value: 'parent 1-1',
-                title: 'parent 1-1',
-                children: [
-                    {
-                        value: 'sss',
-                        title: <b style={{color: '#08c'}}>sss</b>,
-                    },
-                ],
-            },
-        ],
-    },
-];
-
 const CFBestCDN: React.FC = () => {
 
     const [data, setData] = stub.ref.react.useState<any>({})
