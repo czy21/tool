6b1ae6ae13cd74454b012ec78c66a459bf8fff0a
Bruce CHEN <805899926@qq.com>
2024-03-07 21:14:02 +0800

    #

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import {Layout} from "antd";
+import {ConfigProvider, Layout} from "antd";
+import police from "@/assets/image/police.png"
 
 
 const Footer: React.FC = () => {
     return (
-        <Layout.Footer style={{textAlign: 'center'}}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Layout.Footer>
+        <ConfigProvider theme={{
+            token: {
+                colorLink: "#555",
+                colorLinkHover: "#000",
+                linkDecoration: "underline"
+            }
+        }}>
+            <Layout.Footer style={{textAlign: 'center'}}>
+                <div className="beian">
+                    <a href="https://beian.miit.gov.cn" target="_blank">黑ICP备2024018382号-1</a>
+                    <img src={police} alt="黑公网安备"
+                         style={{
+                             display: "inline-block",
+                             margin: "0 3px",
+                             verticalAlign: "middle"
+                         }}/>
+                    <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=23018302000140" target="_blank">黑公网安备 23018302000140 号</a>
+                </div>
+            </Layout.Footer>
+        </ConfigProvider>
     );
 }
 
