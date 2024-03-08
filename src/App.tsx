fc2faff512f8118e3db1005dae4d9bd43158815d
Bruce CHEN <805899926@qq.com>
2024-03-08 20:04:55 +0800

    # improve style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Home from "@/layout/Home";
 import {BrowserRouter} from "react-router-dom";
+import {ConfigProvider} from 'antd';
 
 // const store = createStore(rootReducer);
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
-            <Home/>
+            <ConfigProvider theme={{
+                token: {
+                    colorLink: "#555",
+                    colorLinkHover: "#000",
+                    linkDecoration: "underline",
+                },
+                components: {
+                    Cascader: {
+                        controlItemWidth: 180
+                    }
+                }
+            }}>
+                <Home/>
+            </ConfigProvider>
         </BrowserRouter>
     )
 }
